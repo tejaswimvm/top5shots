22654f91f59ce210566d46bdadd47f606d2fb26a
tejaswi rastogi <tejaswi.mvmbs@gmail.com>
2025-04-24 12:25:45 +0530

    backend url fix

--- src/components/helper/baseurl.jsx
+++ src/components/helper/baseurl.jsx
@@ -1,5 +1,5 @@
 // const base_url = "http://localhost:5000";
-// const base_url = "https://topsbackend-1.onrender.com";
-const base_url = "http://82.25.109.68:5000";
+const base_url = "https://topsbackend-1.onrender.com";
+// const base_url = "http://82.25.109.68:5000";
 
 export default base_url;
